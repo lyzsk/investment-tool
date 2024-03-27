1e34262682c5e614c8d220b7af46e2f1c019601a
lyzsk <sichu.huang2021@outlook.com>
2024-03-27 13:58:16 +0800

    optimize code, fix calculation problem

--- src/main/java/cn/sichu/utils/FinancialCalculationUtil.java
+++ src/main/java/cn/sichu/utils/FinancialCalculationUtil.java
@@ -10,6 +10,35 @@ import java.math.RoundingMode;
 public class FinancialCalculationUtil {
 
     /**
+     * 净申购金额=申购金额/(1+申购费率)
+     * <br/>
+     * 申购费用=申购金额—净申购金额
+     * <br/>
+     * 净申购份额=净申购金额/申购当日基金单位净值
+     * <br/>
+     * 注: 净申购金额及申购份数的计算结果以四舍五入的方法保留小数点后两位
+     *
+     * @param amount amount
+     * @param fee    fee
+     * @param nav    nav
+     * @return java.math.BigDecimal
+     * @author sichu huang
+     * @date 2024/03/18
+     **/
+    public static BigDecimal calculateShare(BigDecimal amount, BigDecimal fee, String nav) {
+        BigDecimal netAmount = amount.subtract(fee);
+        return netAmount.divide(new BigDecimal(nav), 2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * 净申购金额=申购金额/(1+申购费率)
+     * <br/>
+     * 申购费用=申购金额—净申购金额
+     * <br/>
+     * 净申购份额=净申购金额/申购当日基金单位净值
+     * <br/>
+     * 注: 净申购金额及申购份数的计算结果以四舍五入的方法保留小数点后两位
+     *
      * @param amount  amount
      * @param feeRate feeRate
      * @return java.math.BigDecimal
@@ -18,11 +47,17 @@ public class FinancialCalculationUtil {
      **/
     public static BigDecimal calculatePurchaseFee(BigDecimal amount, String feeRate) {
         String rate = feeRate.replace("%", "");
-        BigDecimal r = new BigDecimal(rate).divide(new BigDecimal("100"), 6, RoundingMode.HALF_EVEN);
-        return amount.multiply(r).setScale(2, RoundingMode.HALF_EVEN);
+        BigDecimal netAmount =
+            amount.divide(new BigDecimal("1").add(new BigDecimal(rate).divide(new BigDecimal("100"), 6, RoundingMode.HALF_UP)), 2,
+                RoundingMode.HALF_UP);
+        return amount.subtract(netAmount).setScale(2, RoundingMode.HALF_UP);
     }
 
     /**
+     * 赎回费用＝赎回当日基金单位净值×赎回份额×赎回费率
+     * <br/>
+     * 赎回金额＝赎回当日基金单位净值×赎回份额－赎回费用
+     *
      * @param share   share
      * @param nav     nav
      * @param feeRate feeRate
@@ -33,12 +68,15 @@ public class FinancialCalculationUtil {
     public static BigDecimal calculateRedemptionFee(BigDecimal share, String nav, String feeRate) {
         BigDecimal n = new BigDecimal(nav);
         String rate = feeRate.replace("%", "");
-        BigDecimal r = new BigDecimal(rate).divide(new BigDecimal("100"), 6, RoundingMode.CEILING);
-        BigDecimal amount = share.multiply(n).setScale(2, RoundingMode.CEILING);
-        return amount.multiply(r).setScale(2, RoundingMode.HALF_DOWN);
+        BigDecimal r = new BigDecimal(rate).divide(new BigDecimal("100"), 6, RoundingMode.HALF_UP);
+        return n.multiply(share).multiply(r).setScale(2, RoundingMode.HALF_UP);
     }
 
     /**
+     * 赎回费用＝赎回当日基金单位净值×赎回份额×赎回费率
+     * <br/>
+     * 赎回金额＝赎回当日基金单位净值×赎回份额－赎回费用
+     *
      * @param share share
      * @param nav   nav
      * @param fee   fee
@@ -48,20 +86,7 @@ public class FinancialCalculationUtil {
      **/
     public static BigDecimal calculateRedemptionAmount(BigDecimal share, String nav, BigDecimal fee) {
         BigDecimal n = new BigDecimal(nav);
-        return share.multiply(n).subtract(fee).setScale(2, RoundingMode.CEILING);
+        return share.multiply(n).subtract(fee).setScale(2, RoundingMode.HALF_UP);
     }
 
-    /**
-     * @param amount amount
-     * @param fee    fee
-     * @param nav    nav
-     * @return java.math.BigDecimal
-     * @author sichu huang
-     * @date 2024/03/18
-     **/
-    public static BigDecimal calculateShare(BigDecimal amount, BigDecimal fee, String nav) {
-        BigDecimal n = new BigDecimal(nav);
-        BigDecimal subtract = amount.subtract(fee);
-        return subtract.divide(n, 2, RoundingMode.HALF_EVEN);
-    }
 }
