cc313b4de96751cf9d6d1385a261736cd0c38b24
lyzsk <sichu.huang2021@outlook.com>
2026-05-30 08:39:36 +0800

    update cls api link on 2026/05/30

--- inv-stock/src/main/java/cn/sichu/cls/component/ClsHttpClient.java
+++ inv-stock/src/main/java/cn/sichu/cls/component/ClsHttpClient.java
@@ -31,33 +31,84 @@ public class ClsHttpClient {
             .build();
     }
 
+    // public Mono<String> fetchTelegraphList() {
+    //     MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    //     params.add("app_name", "investment-tool");
+    //     params.add("os", "web");
+    //     params.add("sv", "8.4.6");
+    //     String sign = generateSignForMultiValueMap(params);
+    //     params.add("sign", sign);
+    //
+    //     String queryString = params.entrySet().stream().map(
+    //         entry -> entry.getKey() + "=" + String.join("&" + entry.getKey() + "=",
+    //             entry.getValue())).collect(Collectors.joining("&"));
+    //     String fullUrl = "https://www.cls.cn/nodeapi/updateTelegraphList?" + queryString;
+    //     log.info("即将请求 CLS 电报接口: {}", fullUrl);
+    //
+    //     return webClient.get().uri(
+    //             uriBuilder -> uriBuilder.path("/nodeapi/updateTelegraphList").queryParams(params)
+    //                 .build()).header("User-Agent",
+    //             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    //         .header("Referer", "https://www.cls.cn/")
+    //         .header("Accept", "application/json, text/plain, */*")
+    //         .header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+    //         .header("Accept-Encoding", "gzip, deflate, br").header("Sec-Fetch-Site", "same-origin")
+    //         .header("Sec-Fetch-Mode", "cors").header("Sec-Fetch-Dest", "empty")
+    //         .header("Connection", "keep-alive").retrieve().bodyToMono(String.class)
+    //         .timeout(Duration.ofSeconds(10))
+    //         .doOnSuccess(response -> log.debug("成功获取 CLS 电报数据，长度: {}", response.length()))
+    //         .doOnError(e -> log.error("获取 CLS 电报失败", e));
+    // }
+
+    /**
+     * update 2026/05/30: 从2025/05/29开始404无法访问原接口<a href="https://www.cls.cn/nodeapi/updateTelegraphList">...</a>
+     *
+     * @return reactor.core.publisher.Mono<java.lang.String>
+     * @author sichu huang
+     * @since 2026/05/30 08:30:16
+     */
     public Mono<String> fetchTelegraphList() {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("app_name", "investment-tool");
+        params.add("app", "CailianpressWeb");
+        params.add("name", "telegraph");
         params.add("os", "web");
-        params.add("sv", "8.4.6");
-        String sign = generateSignForMultiValueMap(params);
+        params.add("sv", "8.7.9");
+
+        String sign = generateSign(params);
         params.add("sign", sign);
 
         String queryString = params.entrySet().stream().map(
             entry -> entry.getKey() + "=" + String.join("&" + entry.getKey() + "=",
                 entry.getValue())).collect(Collectors.joining("&"));
-        String fullUrl = "https://www.cls.cn/nodeapi/updateTelegraphList?" + queryString;
+
+        String fullUrl = "https://www.cls.cn/api/cache?" + queryString;
         log.info("即将请求 CLS 电报接口: {}", fullUrl);
 
-        return webClient.get().uri(
-                uriBuilder -> uriBuilder.path("/nodeapi/updateTelegraphList").queryParams(params)
-                    .build()).header("User-Agent",
+        return webClient.get()
+            .uri(uriBuilder -> uriBuilder.path("/api/cache").queryParams(params).build())
+            .header("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
             .header("Referer", "https://www.cls.cn/")
-            .header("Accept", "application/json, text/plain, */*")
-            .header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
-            .header("Accept-Encoding", "gzip, deflate, br").header("Sec-Fetch-Site", "same-origin")
-            .header("Sec-Fetch-Mode", "cors").header("Sec-Fetch-Dest", "empty")
-            .header("Connection", "keep-alive").retrieve().bodyToMono(String.class)
-            .timeout(Duration.ofSeconds(10))
+            .header("Accept", "application/json, text/plain, */*").retrieve()
+            .bodyToMono(String.class).timeout(Duration.ofSeconds(10))
             .doOnSuccess(response -> log.debug("成功获取 CLS 电报数据，长度: {}", response.length()))
             .doOnError(e -> log.error("获取 CLS 电报失败", e));
+    }
+
+    /**
+     * MD5做key=value
+     *
+     * @param params params
+     * @return java.lang.String
+     * @author sichu huang
+     * @since 2026/05/30 08:37:55
+     */
+    private String generateSign(MultiValueMap<String, String> params) {
+        String sortedParams = params.entrySet().stream().sorted(Map.Entry.comparingByKey())
+            .map(entry -> entry.getKey() + "=" + entry.getValue().get(0))
+            .collect(Collectors.joining("&"));
+
+        return DigestUtils.md5Hex(sortedParams);
     }
 
     /**
@@ -89,11 +140,11 @@ public class ClsHttpClient {
      * @author sichu huang
      * @since 2026/01/14 13:39:51
      */
-    private String generateSignForMultiValueMap(MultiValueMap<String, String> params) {
-        String sortedParams = params.entrySet().stream().sorted(Map.Entry.comparingByKey())
-            .map(entry -> entry.getKey() + "=" + entry.getValue().get(0))
-            .collect(Collectors.joining("&"));
-        String sha1 = DigestUtils.sha1Hex(sortedParams);
-        return DigestUtils.md5Hex(sha1);
-    }
+    // private String generateSignForMultiValueMap(MultiValueMap<String, String> params) {
+    //     String sortedParams = params.entrySet().stream().sorted(Map.Entry.comparingByKey())
+    //         .map(entry -> entry.getKey() + "=" + entry.getValue().get(0))
+    //         .collect(Collectors.joining("&"));
+    //     String sha1 = DigestUtils.sha1Hex(sortedParams);
+    //     return DigestUtils.md5Hex(sha1);
+    // }
 }
